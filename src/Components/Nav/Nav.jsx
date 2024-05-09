6021e3ed0cdbb54895d1491ac769d85bd8b292a8
Fabrizio Lora <fabrizio.caleb.lora@gmail.com>
2024-05-09 03:38:49 -0300

    bug

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,20 +1,27 @@
-
 import { Link } from "react-router-dom";
-import styles from './Nav.module.css';
+import styles from "./Nav.module.css";
 
 function Nav() {
-    return(
-        <header>
-        <nav>
-            <ul className={styles.linkPagesContainer}>
-                <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"characters"}>Characters</Link></li>
-                <li><Link to={"/"}>Family Tree</Link></li>
-                <li><Link to={"/"}>Making off</Link></li>
-            </ul>
-        </nav>
+  return (
+    <header>
+      <nav className="border-2">
+        <ul className={styles.linkPagesContainer}>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"characters"}>Characters</Link>
+          </li>
+          <li>
+            <Link to={"/"}>Family Tree</Link>
+          </li>
+          <li>
+            <Link to={"/"}>Making off</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-    );
+  );
 }
 
 export default Nav;
