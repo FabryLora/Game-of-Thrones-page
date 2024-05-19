ed5ea802d4a42ec9768b75820a47a0130ff45967
Fabrizio Lora <fabrizio.caleb.lora@gmail.com>
2024-05-19 00:40:09 -0300

    fixing 19-5

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
           <Link to={"/"} className="mb-36">
             <img src={logo} width="800px" alt="" />
           </Link>
-          <p className="text-4xl"> Who will sit on the iron throne? </p>
+          <p className="mb-5 text-4xl"> Who will sit on the iron throne? </p>
           <div className="flex items-center justify-evenly">
             <iframe
               width="1400"
@@ -33,73 +33,76 @@ function Home() {
         </div>
       </div>
 
+      {/* SECOND HOME SECTION */}
       <div
-        className="flex flex-col items-center justify-center bg-cover bg-center"
+        className="bg-cover bg-center"
         style={{ backgroundImage: `url("${secondSectionImage}")` }}
       >
-        <div className="m-16 flex min-h-72 w-4/5 flex-nowrap items-center justify-evenly rounded-xl bg-[rgba(0,0,0,0.7)] py-8">
-          <Link to={"/"}>
-            <img
-              className="max-h-64"
-              src={familyTreeImage}
-              alt="arbol familiar"
-            />
-          </Link>
-          <div className="flex w-2/4 flex-col">
-            <p className="w-full">
-              Mira el arbol genialogico y enterate de todos los vinculos entre
-              personajes para entender mas a profundidad la historia
-            </p>
-            <a
-              className="w-24 rounded-xl border border-solid border-white bg-none p-2 text-center font-bold text-white hover:bg-white hover:text-black"
-              href="./pages/mackingoff.html"
-            >
-              Ver mas
-            </a>
+        <div className="flex w-full flex-col items-center justify-center bg-[rgba(0,0,0,0.5)]">
+          <div className="m-16 flex min-h-72 w-4/5 flex-nowrap items-center justify-evenly rounded-xl">
+            <Link to={"/"}>
+              <img
+                className="max-h-72 rounded-xl"
+                src={familyTreeImage}
+                alt="arbol familiar"
+              />
+            </Link>
+            <div className="flex w-2/4 flex-col">
+              <p className="w-full">
+                Mira el arbol genialogico y enterate de todos los vinculos entre
+                personajes para entender mas a profundidad la historia
+              </p>
+              <Link
+                className="w-24 rounded-xl border border-solid border-white bg-none p-2 text-center font-bold text-white hover:bg-white hover:text-black"
+                to={"/"}
+              >
+                Ver mas
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div className="m-16 flex min-h-72 w-4/5 flex-nowrap items-center justify-evenly rounded-xl bg-[rgba(0,0,0,0.7)] bg-black py-8">
-          <div className="flex w-2/4 flex-col">
-            <p className="w-full">
-              Mira el arbol genialogico y enterate de todos los vinculos entre
-              personajes para entender mas a profundidad la historia
-            </p>
-            <a
-              className="w-24 rounded-xl border border-solid border-white bg-none p-2 text-center font-bold text-white hover:bg-white hover:text-black"
-              href="./pages/mackingoff.html"
-            >
-              Ver mas
-            </a>
+          <div className="m-16 flex min-h-72 w-4/5 flex-nowrap items-center justify-evenly rounded-xl">
+            <div className="flex w-2/4 flex-col">
+              <p className="w-full">
+                Mira el arbol genialogico y enterate de todos los vinculos entre
+                personajes para entender mas a profundidad la historia
+              </p>
+              <Link
+                className="w-24 rounded-xl border border-solid border-white bg-none p-2 text-center font-bold text-white hover:bg-white hover:text-black"
+                to={"/"}
+              >
+                Ver mas
+              </Link>
+            </div>
+            <Link to={"characters"}>
+              <img
+                className="max-h-72 rounded-xl"
+                src={charactesImage}
+                alt="5 de los personajes de la serie"
+              />
+            </Link>
           </div>
-          <Link to={"/"}>
-            <img
-              className="max-h-64"
-              src={charactesImage}
-              alt="5 de los personajes de la serie"
-            />
-          </Link>
-        </div>
 
-        <div className="m-16 flex min-h-72 w-4/5 flex-nowrap items-center justify-evenly rounded-xl bg-[rgba(0,0,0,0.7)] bg-black py-8">
-          <Link to={"/"}>
-            <img
-              className="max-h-64"
-              src={makingOffImage}
-              alt="foto del macking off"
-            />
-          </Link>
-          <div className="flex w-2/4 flex-col">
-            <p className="w-full">
-              Mira el arbol genialogico y enterate de todos los vinculos entre
-              personajes para entender mas a profundidad la historia
-            </p>
-            <a
-              className="w-24 rounded-xl border border-solid border-white bg-none p-2 text-center font-bold text-white hover:bg-white hover:text-black"
-              href="./pages/mackingoff.html"
-            >
-              Ver mas
-            </a>
+          <div className="m-16 flex min-h-72 w-4/5 flex-nowrap items-center justify-evenly rounded-xl">
+            <Link to={"/"}>
+              <img
+                className="max-h-72 rounded-xl"
+                src={makingOffImage}
+                alt="foto del macking off"
+              />
+            </Link>
+            <div className="flex w-2/4 flex-col">
+              <p className="w-full">
+                Mira el arbol genialogico y enterate de todos los vinculos entre
+                personajes para entender mas a profundidad la historia
+              </p>
+              <Link
+                className="w-24 rounded-xl border border-solid border-white bg-none p-2 text-center font-bold text-white hover:bg-white hover:text-black"
+                to={"/"}
+              >
+                Ver mas
+              </Link>
+            </div>
           </div>
         </div>
       </div>
